9e12e5e977a2946a931e86e943f683c62bd76480
uzair-suria <uzair.a.suria@outlook.com>
2021-11-28 00:15:41 +0500

    margin adjustment

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { MDBInput, MDBCard, MDBBtn } from "mdb-react-ui-kit";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit }) => {
   return (
     <div>
       <MDBCard
         style={{ maxWidth: "600px", margin: "0 auto", padding: "1em" }}
-        className="my-5"
+        className="my-1"
       >
         <div className="d-flex justify-content-center">
           <p>Enter an image link below to detect face</p>
         </div>
         <div>
-          <MDBInput label="Image URL" id="imageUrl" type="text" size="lg" />
+          <MDBInput
+            label="Image URL"
+            id="imageUrl"
+            type="text"
+            size="lg"
+            onChange={onInputChange}
+          />
         </div>
-        <MDBBtn rounded className="my-3 mx-auto">
+        <MDBBtn rounded className="my-3 mx-auto" onClick={onSubmit}>
           Submit
         </MDBBtn>
       </MDBCard>
