6dd3c62debb8ccf82cdc603eb647f57ce6f0dc99
uzair-suria <uzair.a.suria@outlook.com>
2021-11-28 00:16:19 +0500

    added api functionality and introduced state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,104 @@
 import React from "react";
-import { MDBBtn, MDBContainer } from "mdb-react-ui-kit";
+import { MDBContainer } from "mdb-react-ui-kit";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Navigation from "./components/Navigation/Navigation";
+import Rank from "./components/Rank/Rank";
 
-function App() {
-  return (
-    <MDBContainer fluid>
-      <div
-        className="d-flex justify-content-center align-items-center"
-        style={{ height: "100vh" }}
-      >
-        <div className="text-center">
-          <img
-            className="mb-4"
-            src="https://mdbootstrap.com/img/logo/mdb-transparent-250px.png"
-            style={{ width: 250, height: 90 }}
-          />
-          <h5 className="mb-3">
-            Thank you for using our product. We're glad you're with us.
-          </h5>
-          <p className="mb-3">MDB Team</p>
-          <MDBBtn
-            tag="a"
-            href="https://mdbootstrap.com/docs/standard/getting-started/"
-            target="_blank"
-            role="button"
-          >
-            Start MDB tutorial
-          </MDBBtn>
-        </div>
-      </div>
-    </MDBContainer>
-  );
+const initialState = {
+  input: "",
+  imageUrl: "",
+  parsedBoundingBox: {},
+  route: "signin",
+  isSignedIn: false,
+  user: {
+    name: "",
+    email: "",
+    id: "",
+    entries: 0,
+    joined: "",
+  },
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  calculateFaceLocation = (data) => {
+    const rawBoundingBox =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    const box = {
+      top: rawBoundingBox.top_row * height,
+      right: width - rawBoundingBox.right_col * width,
+      bottom: height - rawBoundingBox.bottom_row * height,
+      left: rawBoundingBox.left_col * width,
+    };
+    this.setState({ parsedBoundingBox: box });
+  };
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState(initialState);
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
+  render() {
+    const onSubmit = () => {
+      this.setState({ imageUrl: this.state.input, parsedBoundingBox: {} });
+      fetch("https://uas-face-detection-app.herokuapp.com/imageurl", {
+        method: "post",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({
+          input: this.state.input,
+        }),
+      })
+        .then((res) => {
+          res = res.json().then((data) => {
+            if (data) {
+              fetch("https://uas-face-detection-app.herokuapp.com/image", {
+                method: "put",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify({
+                  id: this.state.user.id,
+                }),
+              })
+                .then((res) => res.json())
+                .then((count) => {
+                  // console.log(count);
+                  this.setState({
+                    user: { ...this.state.user, entries: count },
+                  });
+                });
+            }
+            this.calculateFaceLocation(data);
+          });
+        })
+        .catch((error) => console.log("error", error));
+    };
+    return (
+      <MDBContainer fluid className="px-0">
+        <Navigation
+          onRouteChange={this.onRouteChange}
+          route={this.state.route}
+          isSignedIn={this.state.isSignedIn}
+        />
+        <Rank name="Uzair" count={5} />
+        <ImageLinkForm />
+      </MDBContainer>
+    );
+  }
 }
 
 export default App;
