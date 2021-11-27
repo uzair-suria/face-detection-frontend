ec16fd19e805256acf42713b61b35994a8b00633
uzair-suria <uzair.a.suria@outlook.com>
2021-11-27 23:35:54 +0500

    supplementary styling

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,21 +4,20 @@ import { MDBInput, MDBCard, MDBBtn } from "mdb-react-ui-kit";
 const ImageLinkForm = () => {
   return (
     <div>
-      <div>
-        <MDBCard
-          style={{ maxWidth: "600px", margin: "0 auto", padding: "1em" }}
-        >
-          <div className="d-flex justify-content-center">
-            <p>Enter an image link below to detect face</p>
-          </div>
-          <div>
-            <MDBInput label="Image URL" id="imageUrl" type="text" size="lg" />
-          </div>
-          <MDBBtn rounded className="my-3 mx-auto">
-            Submit
-          </MDBBtn>
-        </MDBCard>
-      </div>
+      <MDBCard
+        style={{ maxWidth: "600px", margin: "0 auto", padding: "1em" }}
+        className="my-5"
+      >
+        <div className="d-flex justify-content-center">
+          <p>Enter an image link below to detect face</p>
+        </div>
+        <div>
+          <MDBInput label="Image URL" id="imageUrl" type="text" size="lg" />
+        </div>
+        <MDBBtn rounded className="my-3 mx-auto">
+          Submit
+        </MDBBtn>
+      </MDBCard>
     </div>
   );
 };
