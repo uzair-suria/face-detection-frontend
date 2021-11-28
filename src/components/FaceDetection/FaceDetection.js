bdeba2861a686512c031291da845ff1612ec834f
uzair-suria <uzair.a.suria@outlook.com>
2021-11-28 15:58:35 +0500

    updated component logic and styling

--- src/components/FaceDetection/FaceDetection.js
+++ src/components/FaceDetection/FaceDetection.js
@@ -1,32 +1,44 @@
+import { MDBCard } from "mdb-react-ui-kit";
 import React from "react";
 
-const Facedetection = ({ imageUrl, box }) => {
-  return (
-    <div className="center">
-      <div className="container">
-        <img
-          className="ma3 shadow-5"
-          src={imageUrl}
-          alt=""
-          style={{ zIndex: 100 }}
-          width="300px"
-          height="auto"
-          id="inputImage"
-        />
+import "./FaceDetection.css";
 
-        {Object.values(box).length !== 0 && (
-          <div
-            className="box"
-            style={{
-              position: "absolute",
-              top: box.top,
-              right: box.right,
-              bottom: box.bottom,
-              left: box.left,
-            }}
-          ></div>
-        )}
-      </div>
-    </div>
+const Facedetection = ({ imageUrl, box }) => {
+  console.log(box);
+  return (
+    <>
+      {imageUrl.length && (
+        <MDBCard
+          style={{ maxWidth: "600px", margin: "0 auto", padding: "1em" }}
+          className="my-1"
+        >
+          <div className="d-flex justify-content-center">
+            <div className="container">
+              <img
+                className=""
+                src={imageUrl}
+                alt={imageUrl ? "image supplied by the user" : ""}
+                width="300px"
+                height="auto"
+                id="inputImage"
+              />
+              {box !== undefined && Object.values(box).length !== 0 && (
+                <div
+                  className="box"
+                  style={{
+                    position: "absolute",
+                    top: box.top,
+                    right: box.right,
+                    bottom: box.bottom,
+                    left: box.left,
+                  }}
+                ></div>
+              )}
+            </div>
+          </div>
+        </MDBCard>
+      )}
+    </>
   );
 };
+export default Facedetection;
