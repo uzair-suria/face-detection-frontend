5405f81262cc1b68b6a0016af18fb84edcd13173
uzair-suria <uzair.a.suria@outlook.com>
2021-11-28 18:25:30 +0500

    removed debuggin console logging

--- src/components/FaceDetection/FaceDetection.js
+++ src/components/FaceDetection/FaceDetection.js
@@ -4,7 +4,7 @@ import React from "react";
 import "./FaceDetection.css";
 
 const Facedetection = ({ imageUrl, box }) => {
-  console.log(box);
+  // console.log(box);
   return (
     <>
       {imageUrl.length && (
